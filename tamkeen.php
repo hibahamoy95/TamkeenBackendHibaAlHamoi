fba211f95d2a8bb4c72e0a5b4f5853029ba70c55
hibahamoy95 <77045551+hibahamoy95@users.noreply.github.com>
2024-10-06 14:18:53 +0300

    initial-commit

---
+++ tamkeen.php
@@ -0,0 +1,24 @@
+
+
+<?php
+class Car {
+    public $brand;
+    public $model;
+    public $color;
+    const WHEELS = 4;
+
+    public function __construct($brand, $model, $color) {
+        $this->brand = $brand;
+        $this->model = $model;
+        $this->color = $color;
+    }
+
+    public function getCarDetails() {
+        return "This car is a {$this->color} {$this->brand} {$this->model} with " . self::WHEELS . " wheels.";
+    }
+}
+
+// Create an instance of the Car class
+$myCar = new Car("Toyota", "Corolla", "Red");
+echo $myCar->getCarDetails();
+?>
